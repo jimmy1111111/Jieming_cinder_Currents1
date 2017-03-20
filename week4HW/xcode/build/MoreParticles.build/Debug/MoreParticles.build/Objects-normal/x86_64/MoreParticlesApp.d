d554f284c84deee9cd540b046db73e82bef3098a
jimmy1111111 <yuj719@newschool.edu>
2017-03-20 15:57:25 -0400

    Add files via upload

---
+++ week4HW/xcode/build/MoreParticles.build/Debug/MoreParticles.build/Objects-normal/x86_64/MoreParticlesApp.d
@@ -0,0 +1,5 @@
+dependencies: \
+  /Users/yujieming/Desktop/cinder/cinder_0.9.0_mac/week4HW/src/MoreParticlesApp.cpp \
+  ../../include/cinder/app/RendererGl.h ../../include/cinder/Rand.h \
+  ../../include/cinder/Perlin.h \
+  /Users/yujieming/Desktop/cinder/cinder_0.9.0_mac/week4HW/xcode/Particle.hpp
---
+++ week4HW/xcode/build/MoreParticles.build/Debug/MoreParticles.build/Objects-normal/x86_64/Particle.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/yujieming/Desktop/cinder/cinder_0.9.0_mac/week4HW/xcode/Particle.cpp \
+  /Users/yujieming/Desktop/cinder/cinder_0.9.0_mac/week4HW/xcode/Particle.hpp \
+  ../../include/cinder/Rand.h
